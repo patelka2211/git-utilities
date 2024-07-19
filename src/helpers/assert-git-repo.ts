45f206a2731aa2b85c76d09acee37600f075d295
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-07-19 23:09:55 +0530

    Updates in `assertGitRepo` function

--- src/helpers/assert-git-repo.ts
+++ src/helpers/assert-git-repo.ts
@@ -5,8 +5,8 @@ export async function assertGitRepo(repoPath: string) {
     try {
         let result = await stat(resolve(repoPath, ".git"));
 
-        if (result.isDirectory() === true) return true;
-        else throw Error(`"${repoPath}" is not a Git repository.`);
+        if (result.isDirectory() !== true)
+            throw Error(`"${repoPath}" is not a Git repository.`);
     } catch {
         throw Error(`"${repoPath}" is not a Git repository.`);
     }
